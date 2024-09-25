d219159122c6843ba892a5ba729264d286b79210
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2024-09-25 14:49:20 +0200

    finita

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,65 @@
-import Profile from "./components/Profile/Profile";
-import userData from "../userData.json";
-import FriendList from "./components/FriendList/FriendList";
-import friends from "../friends.json";
-import transactions from "../transactions.json";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import { useEffect, useState } from "react";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
 
-const App = () => {
+function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const dataLs = localStorage.getItem("feedBack");
+
+    if (!dataLs) {
+      return JSON.parse(dataLs);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+  useEffect(() => {
+    localStorage.setItem("feedBack", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback({
+      ...feedback,
+      [feedbackType]: feedback[feedbackType] + 1,
+    });
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const total = feedback.good + feedback.neutral + feedback.bad;
+
+  const positive = Math.round((feedback.good / total) * 100);
+
   return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
+    <div>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        total={total}
+        resetFeedback={resetFeedback}
       />
-
-      <FriendList friends={friends} />
-
-      <TransactionHistory items={transactions} />
-    </>
+      {total > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={total}
+          positive={positive}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
   );
-};
+}
 export default App;
